43f16a54a3101a75f57a91b1fb275880b55854d4
Igor Stepanov <igorstepanov@hotmail.com>
2022-08-24 12:09:06 +0300

    build(Vue Movies App): настроить axios, создать базовый store, подготовить запросы

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import movies from "./modules/movies";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
-  getters: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    movies,
+  },
 });
