733299f317f47db2966bb4ab59a35a0844cf439d
Igor Stepanov <igorstepanov@hotmail.com>
2022-08-25 11:36:44 +0300

    build(Vue Movies App): реализовать функционал удаления фильма

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -9,7 +9,7 @@ function serializeResponse(movies) {
   }, {});
 }
 
-const { MOVIES, CURRENT_PAGE } = mutations;
+const { MOVIES, CURRENT_PAGE, REMOVE_MOVIE } = mutations;
 
 const moviesStore = {
   namespaced: true,
@@ -35,6 +35,9 @@ const moviesStore = {
     },
     [CURRENT_PAGE](state, value) {
       state.currentPage = value;
+    },
+    [REMOVE_MOVIE](state, index) {
+      state.top250IDs.splice(index, 1);
     },
   },
   actions: {
@@ -65,6 +68,14 @@ const moviesStore = {
     changeCurrentPage({ commit, dispatch }, page) {
       commit(CURRENT_PAGE, page);
       dispatch("fetchMovies");
+    },
+    removeMovie({ commit, dispatch, state }, id) {
+      const index = state.top250IDs.findIndex((item) => item === id);
+
+      if (index !== -1) {
+        commit(REMOVE_MOVIE, index);
+        dispatch("fetchMovies");
+      }
     },
   },
 };
