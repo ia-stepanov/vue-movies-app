d57fbab86cdfeae0a427a50532653f4953d6ed63
Igor Stepanov <igorstepanov@hotmail.com>
2022-08-25 10:14:52 +0300

    build(Vue Movies App): создать пагинацию, реализовать функционал пагинации

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id="app">
     <PosterBg :poster="posterBg" />
     <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+    <MoviesPagination
+      :current-page="currentPage"
+      :per-page="moviesPerPage"
+      :total="moviesLength"
+      @pageChanged="onPageChanged"
+    />
   </div>
 </template>
 
@@ -9,24 +15,34 @@
 import { mapActions, mapGetters } from "vuex";
 import MoviesList from "@/components/MoviesList.vue";
 import PosterBg from "@/components/PosterBg.vue";
+import MoviesPagination from "@/components/MoviesPagination.vue";
 
 export default {
   name: "App",
   components: {
     MoviesList,
     PosterBg,
+    MoviesPagination,
   },
   data: () => ({
     posterBg: "",
   }),
   computed: {
-    ...mapGetters("movies", ["moviesList"]),
+    ...mapGetters("movies", [
+      "moviesList",
+      "currentPage",
+      "moviesPerPage",
+      "moviesLength",
+    ]),
   },
   methods: {
-    ...mapActions("movies", ["fetchMovies"]),
+    ...mapActions("movies", ["changeCurrentPage"]),
     onChangePoster(poster) {
       console.log(poster);
       this.posterBg = poster;
+    },
+    onPageChanged(page) {
+      this.changeCurrentPage(page);
     },
   },
 };
@@ -38,5 +54,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   position: relative;
+  overflow: hidden;
 }
 </style>
